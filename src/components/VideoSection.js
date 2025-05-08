c17929d6ac14235978e7573eca5e755aa74f49a6
thoyoin <lukharchik@gmail.com>
2025-05-08 21:20:27 +0300

    upd

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -1,12 +1,12 @@
-import video from '../images/video.mp4'
-
 const VideoSection = () => {
     return ( 
         <div className="video__wrapper">
             <section className="video__section">
                 <div>АЙДЕНТИКА</div>
-                <video autoPlay muted loop preload="auto" playsinline src={video}></video>
-                {/* <p>АЙДЕНТИКА</p> */}
+                <video autoPlay muted loop preload="auto" playsInline>
+                    <source src="/video.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
             </section>
         </div>
      );
