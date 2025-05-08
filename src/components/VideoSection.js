97984c4ae83d8d12b2daf7b84e288127b44b2741
thoyoin <lukharchik@gmail.com>
2025-05-08 21:42:11 +0300

    upd

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -4,7 +4,7 @@ const VideoSection = () => {
             <section className="video__section">
                 <div>АЙДЕНТИКА</div>
                 <video autoPlay muted loop preload="auto" playsInline>
-                    <source src="/video.mp4" type="video/mp4" />
+                    <source src={process.env.PUBLIC_URL + "/video.mp4"} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </section>
